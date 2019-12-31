7cd9ffec8542cd312c9a42dd5a211fdfbe1fcb0e
Bogdanov Vladimir <bogdanov@t-email.hu>
2019-12-31 10:35:13 +0100

    teszt programok eltavolitasa

--- WrapperSoftware/src/WrapperSoftware.java
+++ WrapperSoftware/src/WrapperSoftware.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class WrapperSoftware {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		new WrapperSoftware().run();
+		
+	}
+
+	private void run() {
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		
+		
+		scanner.close(); //ez a sor mindig utolsó legyen
 	}
 
 }
