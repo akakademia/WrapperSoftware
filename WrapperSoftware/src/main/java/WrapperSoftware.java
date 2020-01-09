dc1a2e78a940b01cacd4e4eaa9088a93c08fb396
Kalix1984 <istvn.horvath@yahoo.com>
2020-01-09 02:27:10 +0100

    Maven project létrehozása...talán most sikerül

---
+++ WrapperSoftware/src/main/java/WrapperSoftware.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WrapperSoftware {
+
+	public static void main(String[] args) {
+
+		new WrapperSoftware().run();
+
+	}
+
+	private void run() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		scanner.close(); // fontos hogy ez a sor mindig legyen utolsó
+	}
+
+}
