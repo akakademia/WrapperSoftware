d203394c1459c757f5b9c2567d8641fe831450b2
Kalix1984 <istvn.horvath@yahoo.com>
2020-01-21 12:05:54 +0100

    TestGetNumber hozzáadva

--- WrapperSoftware/src/test/java/TestGetNumber.java
+++ WrapperSoftware/src/test/java/TestGetNumber.java
@@ -1,62 +1,51 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.Scanner;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestGetNumber {
+	Scanner scan;
 	GetNumber number;
-	private final InputStream systemIn = System.in;
-	private ByteArrayInputStream testIn;
-	
+
 	@BeforeEach
 	void init() {
-		Scanner scan = new Scanner(System.in);
+		String testValue = "2\n";
+		scan = new Scanner(testValue);
 		number = new GetNumber(scan);
 	}
-	
+
+	@AfterEach
+	public void resetSystemIn() {
+		scan = new Scanner(System.in);
+		number = new GetNumber(scan);
+	}
+
 	@Test
 	@DisplayName("getIntegerNumber()")
 	void testGetIntegerNumber() {
 		assertEquals(0, number.getIntegerNumber());
 	}
-	
+
 	@Test
 	@DisplayName("getDoubleNumber()")
 	void testGetDoubleNumber() {
 		assertEquals(0.0, number.getDoubleNumber());
 	}
-	
-//	@Test
-//	@DisplayName("GetValidIntegerFromUser")
-//	void testGetValidIntegerFromUser() {
-//		assertEquals(5, number.getValidIntegerFromUser(0, 10));
-//		
-//	}
-	
 
-//    @AfterAll
-//    public void resetSystemIn() {
-//        System.setIn(systemIn);
-//    }
-
-//    private void inputData(String data) {
-//        testIn = new ByteArrayInputStream(data.getBytes());
-//        System.setIn(testIn);
-//    }
-//
-//    @Test
-//    public void testchoosePlayers() {
-//        inputData("5");
-//        int actual = number.getValidIntegerFromUser(0, 10);
-//        assertEquals(5, actual);
-//     
-//    }
+	@Test
+	@DisplayName("GetValidIntegerFromUser tesztelése System.in megkerülésével, ha Valid")
+	public void testGetValidIntegerFromUserWhenValid() {
+		assertEquals(2, number.getValidIntegerFromUser(0, 10), "ha a bemenet 2 akkor a kimenet 2?");
+	}
+	
+	@Test
+	@DisplayName("getValidDoubleFromUser tesztelése System.in megkerülésével, ha Valid")
+	public void testgetValidDoubleFromUserWhenValid() {
+		assertEquals(2, number.getValidDoubleFromUser(0, 10), "ha a bemenet 2 akkor a kimenet 2?");
+	}
 
 }
