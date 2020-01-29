a43bde6691a7363174cda4968995195af6a32e9c
Kalix1984 <istvn.horvath@yahoo.com>
2020-01-29 20:25:11 +0100

    További tesztek hozzáadva a TestGetInputFromUser-hez

--- WrapperSoftware/src/test/java/wrappersoftware/TestGetInputFromUser.java
+++ WrapperSoftware/src/test/java/wrappersoftware/TestGetInputFromUser.java
@@ -1,59 +1,88 @@
 package wrappersoftware;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Scanner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestGetInputFromUser {
 	Scanner scan;
-	GetInputFromUser input;
-        
-        void makeFakeIntNumberInput(){
-            String testValue = "2\n";
-            scan = new Scanner(testValue);
-            input = new GetInputFromUser(scan);
-        }
-        
-        void makeFakedoubleNumberInput(){
-            String testValue = "2,5\n";
-            scan = new Scanner(testValue);
-            input = new GetInputFromUser(scan);
-        }
-        
-        void makeFakeStringInput(){
-            String testValue = "alma\n";
-            scan = new Scanner(testValue);
-            input = new GetInputFromUser(scan);
-        }
-        
-        @AfterEach
+	GetInputFromUser input1;
+	GetInputFromUser input2;
+	
+
+	public void makeFakeIntNumberInput() {
+		String testValue = "2\n";
+		scan = new Scanner(testValue);
+		input1 = new GetInputFromUser(scan);
+	}
+
+	public void makeFakedoubleNumberInput() {
+		String testValue = "2,5\n";
+		scan = new Scanner(testValue);
+		input1 = new GetInputFromUser(scan);
+	}
+
+	public void makeFakeStringInput() {
+		String testValue = "alma\n";
+		scan = new Scanner(testValue);
+		input1 = new GetInputFromUser(scan);
+	}
+	
+	@BeforeEach
+	public void initInput2() {
+		input2 = new GetInputFromUser(scan, "üzenet");
+	} 
+
+	@AfterEach
 	public void resetSystemIn() {
 		scan = new Scanner(System.in);
-		input = new GetInputFromUser(scan);
+		input1 = new GetInputFromUser(scan);
 	}
-        
-        @Test
-        @DisplayName("getPasswordFromUser() tesztelése")
-        public void testGetPasswordFromUser(){
-            makeFakeStringInput();
-            assertEquals("alma", input.getPasswordFromUser());
-        }
+
+	@Test
+	@DisplayName("getPasswordFromUser() tesztelése")
+	public void testGetPasswordFromUser() {
+		makeFakeStringInput();
+		assertEquals("alma", input1.getPasswordFromUser());
+	}
 
 	@Test
 	@DisplayName("GetValidIntegerFromUser tesztelése System.in megkerülésével, ha Valid")
 	public void testGetValidIntegerFromUserWhenValid() {
-            makeFakeIntNumberInput();
-            assertEquals(2, input.getValidIntegerFromUser(0, 10), "ha a bemenet 2 akkor a kimenet 2?");
+		makeFakeIntNumberInput();
+		assertEquals(2, input1.getValidIntegerFromUser(0, 10), "ha a bemenet 2 akkor a kimenet 2?");
 	}
-	
+
 	@Test
 	@DisplayName("getValidDoubleFromUser tesztelése System.in megkerülésével, ha Valid")
 	public void testgetValidDoubleFromUserWhenValid() {
-            makeFakedoubleNumberInput();
-            assertEquals(2.5, input.getValidDoubleFromUser(0, 10), "ha a bemenet 2.5 akkor a kimenet 2?");
+		makeFakedoubleNumberInput();
+		assertEquals(2.5, input1.getValidDoubleFromUser(0, 10), "ha a bemenet 2.5 akkor a kimenet 2?");
 	}
 
+	@Test
+	@DisplayName("Exception a getValidIntegerFromUser-nél")
+	public void testGetValidIntegerFromUserWhenExceptionGenerated() {
+		makeFakeStringInput();
+		assertThrows(Exception.class, () -> {
+			input1.getValidIntegerFromUser(0, 10);
+		});
+	}
+	
+	@Test
+	@DisplayName("Exception a getValidDoubleFromUser-nél")
+	public void testGetValidDoubleFromUserWhenExceptionGenerated() {
+		makeFakeStringInput();
+		assertThrows(Exception.class, () -> {
+			input1.getValidDoubleFromUser(0, 10);
+		});
+	}
+
+	
+	
 }
